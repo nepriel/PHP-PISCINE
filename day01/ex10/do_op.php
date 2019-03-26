3e5160f48b6df70800478bd9c6daedaae0977da0
salut <salut@lala.fr>
2019-03-26 19:58:20 +0100

    yes

---
+++ day01/ex10/do_op.php
@@ -0,0 +1,20 @@
+#!/usr/bin/php
+<?PHP
+if ($argc == 4)
+{
+	$a = trim($argv[1]);
+	$b = trim($argv[3]);
+	$oper = trim($argv[2]);
+	if ($oper == "+")
+		$res = $a + $b;
+	else if ($oper == "-")
+		$res = $a - $b;
+	else if ($oper == "*")
+		$res = $a * $b;
+	else if ($oper == "/")
+		$res = $a / $b;
+	else if ($oper == "%")
+		$res = $a % $b;
+	echo $res."\n";
+}
+?>
