9e9b2a168a201f7cb07f729e238028cddb64d672
salut <salut@lala.fr>
2019-03-26 22:26:06 +0100

    yep

--- day01/ex11/do_op_2.php
+++ day01/ex11/do_op_2.php
@@ -1,31 +1,26 @@
 #!/usr/bin/php
 <?PHP
-
 if ($argc != 2)
 	echo "Incorrect Parameters";
-
-//$oper = explode(",", "+,-,*,/,%");
 else
 {
-$test = sscanf($argv[1], "%d %c %d %s");
-if ($test[0] && $test[1] && $test[2] && !$test[3])
-{
-	if($test[1] == '*')
-		$result = $test[0] * $test[2];
-	if($test[1] == '-')
-		$result = $test[0] - $test[2];
-	if($test[1] == '/')
-		$result = $test[0] / $test[2];
-	if($test[1] == '%')
-		$result = $test[0] % $test[2];
-	if($test[1] == '+')
-		$result = $test[0] + $test[2];
-	echo $result;
-	echo "\n";
-}
-else
-{
-	echo "Syntax Error\n";
-}
+	$ope = sscanf($argv[1], "%d %c %d %s");
+	if ($ope[0] && $ope[1] && $ope[2] && !$ope[3])
+	{
+		if($ope[1] == '*')
+			$result = $ope[0] * $ope[2];
+		if($ope[1] == '-')
+			$result = $ope[0] - $ope[2];
+		if($ope[1] == '/')
+			$result = $ope[0] / $ope[2];
+		if($ope[1] == '%')
+			$result = $ope[0] % $ope[2];
+		if($ope[1] == '+')
+			$result = $ope[0] + $ope[2];
+		echo $result;
+		echo "\n";
+	}
+	else
+		echo "Syntax Error\n";
 }
 ?>
---
+++ day01/ex12/search_it!.php
@@ -0,0 +1,27 @@
+#!/usr/bin/php
+<?PHP
+
+if ($argc >= 2)
+{
+	$str = $argv[1];
+	unset($argv[0]);
+	unset($argv[1]);
+	$i = 0;
+	$j = 0;
+	$stack = array();
+	foreach($argv as $elem)
+	{	
+		$tmp = explode(":", $elem);
+		$stack = array_merge($stack, $tmp);
+		if (strpos($stack[($i * 2)], $str) !== FALSE)
+		{
+			$j++;
+			$good_index = ($i * 2) + 1;
+		}
+		$i++;
+	}
+	if ($j != 0)
+		echo $stack[$good_index]."\n";
+}
+
+?>
